be9fd86b4d0d7bc9183f9f1e71e83d0179f0132a
Candice <candice.poux-pichery@epitech.eu>
2021-07-15 23:39:22 +0200

    public

---
+++ src/data.js
@@ -0,0 +1,61 @@
+export const projects = [
+    {
+        title: "My Quiz",
+        subtitle: "Symfony",
+        description:
+            "Réalisation d'un site de Quiz",
+        image: "./quiz.jpg",
+        link: "https://github.com/Candicepx",
+    },
+    {
+        title: "FreeAds",
+        subtitle: "Laravel",
+        description:
+            "Réalisation d'un site d'annonces",
+        image: "./annonces.jpg",
+        link: "https://github.com/Candicepx",
+    },
+    {
+        title: "PiePHP",
+        subtitle: "PHP",
+        description:
+            "Création d'un framework MVC en PHP?",
+        image: "./mvc.png",
+        link: "https://github.com/Candicepx",
+    },
+    {
+        title: "PiePHP",
+        subtitle: "PHP",
+        description:
+            "Création d'un framework MVC en PHP?",
+        image: "./iot.jpg",
+        link: "https://github.com/Candicepx",
+    },
+];
+
+export const skills = [
+    {
+        name: "HTML",
+    },
+    {
+        name: "CSS",
+    },
+    {
+        name: "PHP",
+    },
+    {
+        name: "Laravel",
+    },
+    {
+        name: "Symfony",
+    },
+    {
+        name: "Javascript",
+    },
+    {
+        name: "ReactJS",
+    },
+    {
+        name: "Photoshop",
+    },
+];
